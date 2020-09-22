69127844a7be8e0d0b7a9da662cc4378799e15aa
Robert Stevens <robertstevens2015@gmail.com>
2020-09-22 18:55:39 -0500

    initial forms are set up!

--- my-app/src/Components/Login.js
+++ my-app/src/Components/Login.js
@@ -2,6 +2,30 @@ import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 
-const Login;
+const Login = () => {
+    <div>
+        <h1>Log in to your account!</h1>
+        <Formik
+            initialValues={{
+                email: '',
+                password: ''
+            }}
+            onSubmit={async values => {
+                await new Promise(r => setTimeout(r, 500));
+                alert(JSON.stringify(values, null, 2));
+            }}
+        >
+            <Form>
+                <label htmlFor='email'>Email:</label>
+                <Field id='email' name='email' placeholder='Enter your email!' type='email'/>
+
+                <label htmlFor='password'>Password</label>
+                <Field id='password' name='password' placeholder='Enter your password!'/>
+
+                <button type='submit'>Submit</button>
+            </Form>
+        </Formik>
+    </div>
+};
 
 export default Login;
--- my-app/src/Components/Register.js
+++ my-app/src/Components/Register.js
@@ -1,7 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
+import {Formik, Field, Form} from Formik;
 
-const Register;
+
+const Register = () => {
+    <div>
+        <h1>Register!</h1>
+        <Formik
+            initialValues={{
+                name: '',
+                email: '',
+                username: '',
+                password: '',
+            }}
+            onSubmit={async values => {
+                await new Promise(r => setTimeout(r, 500));
+                alert(JSON.stringify(values, null, 2));
+            }}
+        >
+            <Form>
+                <label htmlFor='name'>Name:</label>
+                <Field id='name' name='name' placeholder='Enter your name!'/>
+                
+                <label htmlFor='username'>Username:</label>
+                <Field id='username' name='username' placeholder='Pick a username!'/>
+
+                <label htmlFor='email'>Email:</label>
+                <Field id='email' name='email' placeholder='Enter your email!' type='email'/>
+
+                <label htmlFor='password'>Password</label>
+                <Field id='password' name='password' placeholder='Choose a password!'/>
+
+                <button type='submit'>Submit</button>
+            </Form>
+        </Formik>
+    </div>
+};
 
 export default Register;
