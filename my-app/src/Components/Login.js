f28fe4e711d620307534c719b0aba256abb995ee
Robert Stevens <robertstevens2015@gmail.com>
2020-09-23 20:18:25 -0500

    Login/signup pages created and working

--- my-app/src/Components/Login.js
+++ my-app/src/Components/Login.js
@@ -1,8 +1,25 @@
 import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
+import {Formik, Field, Form} from 'formik';
+import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
+    const submit = submission => {
+        submission.preventDefault();
+        const newTeamMember = {
+            email: submission.target.email.value,
+            password: submission.target.password.value
+        }
+        axios.post('https://lambdabw-virtual-backend.herokuapp.com/users/auth/login', newTeamMember)
+        .then(res => {
+            console.log(res.data)
+        })
+        .catch(e => {
+            console.log(e)
+        })
+    }
+    return (
     <div>
         <h1>Log in to your account!</h1>
         <Formik
@@ -10,12 +27,12 @@ const Login = () => {
                 email: '',
                 password: ''
             }}
-            onSubmit={async values => {
-                await new Promise(r => setTimeout(r, 500));
-                alert(JSON.stringify(values, null, 2));
-            }}
+            // onSubmit={async values => {
+            //     await new Promise(r => setTimeout(r, 500));
+            //     alert(JSON.stringify(values, null, 2));
+            // }}
         >
-            <Form>
+            <Form onSubmit={submit}>
                 <label htmlFor='email'>Email:</label>
                 <Field id='email' name='email' placeholder='Enter your email!' type='email'/>
 
@@ -26,6 +43,6 @@ const Login = () => {
             </Form>
         </Formik>
     </div>
-};
+)};
 
 export default Login;
--- my-app/src/Components/Register.js
+++ my-app/src/Components/Register.js
@@ -1,41 +1,76 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
-import {Formik, Field, Form} from Formik;
+import {Formik, Field, Form} from 'formik';
 
 
-const Register = () => {
+const Register = (props) => {
+    const [user, setUser] = useState({
+        userRole: 'user',
+        username: '',
+        email: '',
+        password: ''
+    });
+    const submit = submission => {
+        submission.preventDefault();
+        const newTeamMember = {
+            userRole: submission.target.userRole.value,
+            username: submission.target.username.value,
+            email: submission.target.email.value,
+            password: submission.target.password.value
+        }
+        axios.post('https://lambdabw-virtual-backend.herokuapp.com/users', newTeamMember)
+        .then(res => {
+            console.log(res.data)
+        })
+        .catch(e => {
+            console.log(e)
+        })
+    };
+    const change = e => {
+        setUser({...user, [e.target.name]: e.target.value});
+    }
+    return (
     <div>
         <h1>Register!</h1>
         <Formik
             initialValues={{
-                name: '',
+                firstName: '',
                 email: '',
                 username: '',
                 password: '',
             }}
-            onSubmit={async values => {
-                await new Promise(r => setTimeout(r, 500));
-                alert(JSON.stringify(values, null, 2));
-            }}
+            // onSubmit={async values => {
+            //     await new Promise(r => setTimeout(r, 500));
+            //     alert(JSON.stringify(values, null, 2));
+            // }}
         >
-            <Form>
-                <label htmlFor='name'>Name:</label>
-                <Field id='name' name='name' placeholder='Enter your name!'/>
+            <Form onSubmit={submit}>
+                <label htmlFor='userRole'>Role:</label>
+                <Field as='select' name='userRole' onChange={change}>
+                    <option value='user'>
+                        User
+                    </option>
+                    <option value='investor'>
+                        Investor
+                    </option>
+                </Field>
+                {/* <Field id='name' name='firstName' placeholder='Enter your name!' onChange={change} value={user.firstName}/> */}
                 
                 <label htmlFor='username'>Username:</label>
-                <Field id='username' name='username' placeholder='Pick a username!'/>
+                <Field id='username' name='username' placeholder='Pick a username!' onChange={change} value={user.username}/>
 
                 <label htmlFor='email'>Email:</label>
-                <Field id='email' name='email' placeholder='Enter your email!' type='email'/>
+                <Field id='email' name='email' placeholder='Enter your email!' type='email' onChange={change} value={user.email}/>
 
                 <label htmlFor='password'>Password</label>
-                <Field id='password' name='password' placeholder='Choose a password!'/>
+                <Field id='password' name='password' placeholder='Choose a password!' onChange={change} value={user.password}/>
 
                 <button type='submit'>Submit</button>
             </Form>
         </Formik>
     </div>
-};
+    )};
 
 export default Register;
